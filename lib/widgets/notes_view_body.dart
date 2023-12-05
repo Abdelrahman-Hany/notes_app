1c81fac771c346099ebf3715cbea9be68249a61c
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2023-12-05 20:54:18 +0200

    create notes list view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -8,16 +8,17 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16),
-      child:  Column(
-        children:[
-          SizedBox(height: 30,),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
           CustomAppBar(),
-          NoteItem(),
+           Expanded(child: NotesListView()),
         ],
       ),
     );
   }
 }
-
 
