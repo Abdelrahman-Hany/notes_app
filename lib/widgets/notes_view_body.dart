29d5f3c5874875037b8c13b6aee2c903efa65d36
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2023-12-05 23:02:12 +0200

    add border radius to bottom sheet

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -12,7 +12,7 @@ class NotesViewBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(
-            height: 30,
+            height: 35,
           ),
           CustomAppBar(),
            Expanded(child: NotesListView()),
