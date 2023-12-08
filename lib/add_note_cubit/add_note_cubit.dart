5b564a7ee9c777c016870b61dcf1a6465afe7d6e
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2023-12-08 20:31:56 +0200

    add note methode to add note cubit

--- lib/add_note_cubit/add_note_cubit.dart
+++ lib/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/add_note_cubit/add_note_state.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/models/note_model.dart';
 
 class NotesCubit extends Cubit<AddNoteState> {
   NotesCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    }catch (e) {
+      AddNoteFailure(errorMessage: e.toString());
+    }
+  }
 }
+
+// add<T>(T name) {//genaric fanction is used to when calling the function you spcify the type of the data the function takes
+//   return name;
+// }
