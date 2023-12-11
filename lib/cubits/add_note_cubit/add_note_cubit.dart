00585679857ad83443f78a31315a0b03960c3ca9
Abdelrahman-Hany <abdelrahman.hany013@gmail.com>
2023-12-12 00:05:20 +0200

    create notes state

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:note_app/add_note_cubit/add_note_state.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
 
